35484f4cd780f2282c1d7aa30bcfab7b131c5586
Bianca Melaré <105999176+Biancamelare@users.noreply.github.com>
2023-09-18 13:02:36 -0300

    Update media.js

--- Atividade7/media.js
+++ Atividade7/media.js
@@ -1,7 +1,7 @@
 var nome = prompt("Qual é o seu nome?");
-var nota1 = prompt("Digute a 1° nota?");
-var nota2 = prompt("Digute a 2° nota?");
-var nota3 = prompt("Digute a 3° nota?");
+var nota1 = prompt("Digite a 1° nota:");
+var nota2 = prompt("Digite a 2° nota:");
+var nota3 = prompt("Digite a 3° nota:");
 
 var media = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3))/3;
 
